6d324e2fba02f78a46ca1114a1d407cdf01d29a7
udayraj1238 <rajuday6002@gmail.com>
2026-07-01 11:41:51 +0530

    Fix DockerWorker arguments in run_worker.py

--- run_worker.py
+++ run_worker.py
@@ -12,7 +12,7 @@ async def main():
     port = int(os.environ.get('BROKER_PORT', '5555'))
     queue = os.environ.get('QUEUE_NAME', 'tasks')
     worker_id = os.environ.get('WORKER_ID', f'worker-{os.getpid()}')
-    w = DockerWorker(broker_host=host, broker_port=port, worker_id=worker_id, queues=[queue])
+    w = DockerWorker(queue_name=queue, host=host, port=port, worker_id=worker_id)
     await w.start()
 
 if __name__ == '__main__':
